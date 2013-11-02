fae02b6fb79b2a39a2f273efd141ab3fbc4e647a
AlphaTr <alphatr@qq.com>
2013-11-02 22:13:34 +0800

    修改选项页

--- src/options/script.js
+++ src/options/script.js
@@ -1,43 +1,68 @@
-var config = {
-	tinyUrl: {
-		enabled: true, // 是否转换为短网址
-		type: 'baidu' // 短网址接口
-	},
-	qrCode: {
-		type: 'liantu' // 二维码接口
-	}
-}
-var saveOption = function() {
-	config.tinyUrl.enabled = $('#tinyUrlEnabled').get(0).checked;
-	config.tinyUrl.type = $('#tinyUrlType').val();
-	config.qrCode.type = $('#qrCodeType').val();
-	
-	localStorage["tinyQrcode"] = JSON.stringify( config );
-	console.log( JSON.stringify( config ) );
-	$('.info p').html("保存成功").fadeIn(400);
-	setTimeout( function(){
-		$('.info p').fadeOut(400);
-	}, 1000);
-};
-(function(){ // loadOption
-	var saveConfig = localStorage["tinyQrcode"] ? JSON.parse( localStorage["tinyQrcode"] ) : config;
-	if( !saveConfig.tinyUrl.enabled ) {
-		$('#tinyUrlEnabled').removeAttr("checked");
-		$('#tinyUrlType').attr("disabled", "disabled");
-	};
-	$('#tinyUrlType').val( saveConfig.tinyUrl.type );
-	$('#qrCodeType').val( saveConfig.qrCode.type );
-}());
-$('button').on( 'click', function() {
-	saveOption();
-});
-$('.info p').on('click', function(){
-	$(this).fadeOut(400);
-});
-$("#tinyUrlEnabled").on("change", function() {
-	if( this.checked ) {
-		$('#tinyUrlType').removeAttr("disabled");
-	} else {
-		$('#tinyUrlType').attr("disabled", "disabled");
-	}
-})
+(function(){
+    var config = {
+        tinyUrl: {
+            enabled: true, // 是否转换为短网址
+            type: 'baidu' // 短网址接口
+        },
+        qrCode: {
+            type: 'liantu' // 二维码接口
+        }
+    },
+    
+    // 弹出信息
+    showInfo = function( text, type ) {
+        type = type || 'success';
+        $('.info').addClass( type ).html( text ).fadeIn(300, function(){
+            setTimeout( function(){
+                $('.info').fadeOut(300);
+            }, 1000);
+        });
+    },
+    
+    // 初始化选项
+    initOption = function() {
+        config = localStorage["Uqrcode"] ? JSON.parse( localStorage["Uqrcode"] ) : config;
+        if( !config.tinyUrl.enabled ) {
+            $('#tinyUrlEnabled').removeAttr("checked");
+            $('#tinyUrlType').attr("disabled", "disabled");
+        };
+        $('#tinyUrlType').val( config.tinyUrl.type );
+        $('#qrCodeType').val( config.qrCode.type );
+    },
+    
+    // 保存选项
+    saveOption = function() {
+        localStorage["Uqrcode"] = JSON.stringify( config );
+        showInfo("保存配置信息成功！");
+    };
+    
+    initOption();
+    
+    // 单击消失
+    $('.info').on('click', function(){
+        $(this).fadeOut(100);
+    });
+    
+    // 使用短网址
+    $("#tinyUrlEnabled").on("change", function() {
+        if( this.checked ) {
+            $('#tinyUrlType').removeAttr("disabled");
+        } else {
+            $('#tinyUrlType').attr("disabled", "disabled");
+        }
+        config.tinyUrl.enabled = this.checked;
+        saveOption();
+    });
+    
+    // 短网址类型
+    $("#tinyUrlType").on("change", function() {
+        config.tinyUrl.type = $(this).val();
+        saveOption();
+    });
+    
+    // 二维码服务商
+    $("#qrCodeType").on("change", function() {
+        config.qrCode.type = $(this).val();
+        saveOption();
+    });
+})();
