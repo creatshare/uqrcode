7725415b66601f842f53df621a9bd7b123f49b7d
AlphaTr <trefoiles@gmail.com>
2014-03-31 15:41:35 +0800

    modify qrcode

--- src/content.js
+++ src/content.js
@@ -1,69 +1,43 @@
-(function(){
-    var loadImg = chrome.extension.getURL('images/loading.gif'),
-    dom = '<div id="uqrcode" style="display:none"><div class="uqrcode-border"><span class="uqrcode-close">&times;</span><div class="uqrcode-content"><p class="uqrcode-text">正在玩命加载中<span class="uqrcode-dots">...</span></p><div class="uqrcode-img"><img src="###" width="200" height="200"/></div><div class="uqrcode-loading"><img src="' + loadImg + '" width="30" height="30"/></div></div></div></div>',
-    
-    // 加载动画...
-    loading = function() {
-        var api = {
-            i: 0,
-            text : ['&nbsp;&nbsp;&nbsp;', '.&nbsp;&nbsp;', '..&nbsp;', '...'],
-            timer : setInterval( function() {
-                if ( api.i == api.text.length ) {
-                    api.i = 0;
-                } else {
-                    api.i++;
-                }
-                $('#uqrcode p.uqrcode-text .uqrcode-dots').html( api.text[api.i] );
-            }, 400)
-        };
-        
-        return api;
-    },
+/* global chrome */
+(function () {
+    var dom = '<div id="uqrcode" style="display:none"><div class="uqrcode-border"><span class="uqrcode-close">&times;</span><div class="uqrcode-content"><div class="uqrcode-img"><img></div></div></div></div>',
 
-    showQrcode = function() {
-        chrome.extension.sendMessage( { 'type': 'hideContextMenus' } );
-        loading();
-        $('body').append( dom );
-        $('#uqrcode').fadeIn(100);
-        
-        $("#uqrcode .uqrcode-close").on("click", function() {
-            hideQrcode();
-        });
-        $("#uqrcode").on("click", function( e ) {
-            // console.log( "close ");
-            if( e.target == this ) {
+        showQrcode = function (data) {
+            chrome.extension.sendMessage({ 'type': 'hideContextMenus' });
+            $('body').append(dom);
+            $('#uqrcode').fadeIn(100);
+            setValue(data);
+
+            $("#uqrcode .uqrcode-close").on("click", function () {
                 hideQrcode();
-            }
-        });
-    },
-    
-    setValue = function( data ) {
-        var qrcode = new Image();
-        qrcode.src = data.imgUrl;
-        qrcode.onload = function() {
-            clearInterval( loading.timer );
-            $('#uqrcode .uqrcode-img').show();
-            $('#uqrcode .uqrcode-loading').hide();
-        }
-        $('#uqrcode .uqrcode-content').attr('title', data.title );
-        $('#uqrcode .uqrcode-text').html( data.text );
-        $('#uqrcode .uqrcode-img img').attr( 'src', data.imgUrl );
-    },
+            });
 
-    hideQrcode = function() {
-        // console.log('hide');
-        chrome.extension.sendMessage( { 'type': 'showContextMenus' } );
-        $('#uqrcode').fadeOut(100, function() {
-            $('#uqrcode').remove();
-        });
-    };
+            $("#uqrcode").on("click", function (e) {
+                // console.log( "close ");
+                if (e.target === this) {
+                    hideQrcode();
+                }
+            });
+        },
 
-    chrome.extension.onMessage.addListener( function(request, sender, sendResponse ) {
+        setValue = function (data) {
+            $('#uqrcode .uqrcode-content').attr('title', data.title);
+            $('#uqrcode .uqrcode-text').html(data.text);
+            $('#uqrcode .uqrcode-img img').attr('src', data.qrcode);
+        },
+
+        hideQrcode = function () {
+            // console.log('hide');
+            chrome.extension.sendMessage({ 'type': 'showContextMenus' });
+            $('#uqrcode').fadeOut(100, function () {
+                $('#uqrcode').remove();
+            });
+        };
+
+    chrome.extension.onMessage.addListener(function (request) {
         // console.log(request);
-        if( request.type == 'showpage' ) {
-            showQrcode();
-        } else if( request.type == 'setdata' ) {
-            setValue( request.data );
+        if (request.type === 'qrencode') {
+            showQrcode(request.data);
         }
     });
 })();
--- src/main.js
+++ src/main.js
@@ -1,258 +1,126 @@
-var config = {};
+/* global chrome */
+var menuId = {'image': 0, 'text': 0, 'link': 0},
 
+    // 检查网址是否合法
+    cleckUrl = function (url) {
+        var regExp = /^http/;
+        if (url.match(regExp)) {
+            return true;
+        } else {
+            return false;
+        }
+    },
 
-var menuId = {
-	'image': 0,
-	'text': 0,
-	'link': 0
-}
-// 初始化右键菜单
-var initContentMenus = function() {
-	var createTextInfo = {
-		type: 'normal',
-		title: "转化\"%s\"为二维码",
-		contexts: ['selection'],
-		onclick: function( info, tab ) {
-			// console.log("showpage ");
-			chrome.tabs.sendMessage( tab.id, { 'type': 'showpage' } );
-			config = JSON.parse( localStorage['Uqrcode'] );
-			creatData( { "type": 'text', "url": tab.url, "text": info.selectionText }, function( data ) {
-				chrome.tabs.sendMessage( tab.id, { 'type': 'setdata', 'data': data } );
-			});
-		}
-	};	
-	var createImageInfo = {
-		type: 'normal',
-		title: "转化图片地址为二维码",
-		contexts: ['image'],
-		onclick: function( info, tab ) {
-			chrome.tabs.sendMessage( tab.id, { 'type': 'showpage' } );
-			config = JSON.parse( localStorage['Uqrcode'] );
-			creatData( { "type": 'image', "url": info.srcUrl, "title": info.srcUrl }, function( data ) {
-				chrome.tabs.sendMessage( tab.id, { 'type': 'setdata', 'data': data } );
-			});
-		}
-	};
-	
-	var createLinkInfo = {
-		type: 'normal',
-		title: "转化链接为二维码",
-		contexts: ['link'],
-		onclick: function( info, tab ) {
-			chrome.tabs.sendMessage( tab.id, { 'type': 'showpage' } );
-			config = JSON.parse( localStorage['Uqrcode'] );
-			creatData( { "type": 'link', "url": info.linkUrl, "title": info.linkUrl }, function( data ) {
-				chrome.tabs.sendMessage( tab.id, { 'type': 'setdata', 'data': data } );
-			});
-		}
-	};
-	if( !menuId.text ) {
-		menuId.text = chrome.contextMenus.create( createTextInfo );
-	}
-	if( !menuId.image ) {
-		menuId.image = chrome.contextMenus.create( createImageInfo );
-	}
-	if( !menuId.link ) {
-		menuId.link = chrome.contextMenus.create( createLinkInfo );
-	}
-	// console.log( menuId );
-}
+    // 生成二维码
+    creatQrcode = function (argu, callback) {
+        // console.log( "Data =====", val );
+        var data = {},
+            titleList = {
+                'text' : '扫一下，文字瞬间到手机！',
+                'url'  : '扫一下，网址瞬间到手机！',
+                'image': '扫一下，图片瞬间到手机！',
+                'link' : '扫一下，链接瞬间到手机！',
+                'error': '错误：不是有效的网址'
+            },
+            qrcanvas = $('<div>');
 
-// 初始化插件
-var initExt = function() {
-	
-	// 初始化默认选项
-	var defaultConfig = {
-		tinyUrl: {
-			enabled: true, // 是否转换为短网址
-			type: 'baidu' // 短网址接口
-		},
-		qrCode: {
-			type: 'liantu' // 二维码接口
-		}
-	}
-	if( !localStorage['Uqrcode'] ) {
-		localStorage['Uqrcode'] = JSON.stringify( defaultConfig );
-	}
-	config = JSON.parse( localStorage['Uqrcode'] );
-	if( config.showType == 'page' ) {
-		chrome.browserAction.setPopup({'popup':''});
-	} else if( config.showType == 'popup' ) {
-		chrome.browserAction.setPopup({'popup':'./popup.html'});
-	}
-	
-	initContentMenus();
-	chrome.browserAction.onClicked.addListener( function( tab ) {
-		chrome.tabs.sendMessage( tab.id, { 'type': 'showpage' } );
-		config = JSON.parse( localStorage['Uqrcode'] );
-		creatData( { "type": 'url', "url": tab.url, "title": tab.title }, function( data ) {
-			chrome.tabs.sendMessage( tab.id, { 'type': 'setdata', 'data': data } );
-		});
-	});
-	
-	chrome.extension.onMessage.addListener( function(request, sender, sendResponse ) {
-		if( request.type == 'hideContextMenus' ) {
-			chrome.contextMenus.removeAll(function(){
-				menuId = { 'image': 0, 'text': 0 };
-			});
-		} else if( request.type == 'showContextMenus' ) {
-			initContentMenus();
-		}
-	});
-}
+        data.type = argu.type;
+        data.text = argu.text;
 
-// Ajax POST 请求
-var AJAXpost = function( type, url, params, callback ) {
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function() {
-		if ( request.readyState == 4 ) {
-			if ( request.status == 200 ) {
-				callback( request.responseText );
-			}
-		}
-	}
-	request.open("POST", url, true);
-	if( type == 1 ) {
-		request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-		params = json2data( params );
-	} else {
-		request.setRequestHeader("Content-Type", "application/json");
-		params = JSON.stringify( params );
-	}
-	request.send( params );
-}
+        qrcanvas.qrcode({text: data.text});
+        setTimeout(function () {
+            data.qrcode = qrcanvas.find('canvas')[0].toDataURL("image/png");
 
-// JSON 对象转换为 formData 字符串
-var json2data = function( json ) {
-	var str = JSON.stringify( json );
-	str = str.replace(/[\{\"\}]/ig,'');
-	str = str.replace(':','=');
-	str = str.replace(',','&');
-	return str;
-}
+            if (!cleckUrl(argu.url)) {
+                data.type = 'error';
+                data.text = '<span class="error">错误：不是有效的网址</span>';
+                data.qrcode = undefined;
+            }
 
-// 获取短网址链接
-var getTinyUrl = function( url, type, callback ) {
-	type = type ? type : 'baidu';
-	switch( type ) {
-		case 'baidu' : {
-			AJAXpost('1', 'http://dwz.cn/create.php',{ 'url': url }, function( val ){
-				val = JSON.parse( val );
-				if( !val.status ) {
-					callback( val.tinyurl );
-				} else {
-					callback( url );
-				}
-			});
-			break;
-		}
-		case 'google' : {
-			AJAXpost('2', 'https://www.googleapis.com/urlshortener/v1/url',{ 'longUrl': url }, function( val ){
-				val = JSON.parse( val );
-				if( val.id ) {
-					callback( val.id );
-				} else {
-					callback( url );
-				}
-			});
-			break;
-		}
-	}
-}
+            data.title = titleList[data.type];
+            console.log(data);
+            callback(data);
+        }, 20);
+    },
 
-// 获取二维码图片链接
-var getQrCode = function( text, type ) {
-	var returnText = '';
-	type = type ? type : 'liantu';
-	switch( type ) {
-		case 'liantu' : {
-			returnText = 'http://qr.liantu.com/api.php?w=200&el=l&m=25&text=' + text;
-			break;
-		}
-		case 'google' : {
-			returnText = 'https://chart.googleapis.com/chart?cht=qr&chs=200x200&choe=UTF-8&chld=L|4&chl=' + text;
-			break;
-		}
-		case 'kuaipai' : {
-			returnText = 'http://api.kuaipai.cn/qr?chs=200x200&chl=' + text;
-			break;
-		}
-	}
-	return returnText;
-}
+    // 注册右键菜单
+    initContentMenus = function () {
+        var createTextInfo = {
+                type: 'normal',
+                title: "转化\"%s\"为二维码",
+                contexts: ['selection'],
+                onclick: function (info, tab) {
+                    // console.log("showpage ");
+                    creatQrcode({"type": 'text', "url": tab.url, "text": info.selectionText}, function (data) {
+                        chrome.tabs.sendMessage(tab.id, {'type': 'qrencode', 'data': data});
+                    });
+                }
+            },
 
-// 检查网址是否合法
-var cleckUrl = function( url ) {
-	var regExp = /^http/;
-	if( url.match( regExp ) ) {
-		return true;
-	} else {
-		return false;
-	}
-}
+            createImageInfo = {
+                type: 'normal',
+                title: "转化图片地址为二维码",
+                contexts: ['image'],
+                onclick: function (info, tab) {
+                    creatQrcode({ "type": 'image', "url": tab.url, "text": info.srcUrl}, function (data) {
+                        chrome.tabs.sendMessage(tab.id, {'type': 'qrencode', 'data': data});
+                    });
+                }
+            },
 
-// 转换为数据
-var creatData = function( val, callback ) {
-	// console.log( "Data =====", val );
-	var data = {};
-	if( cleckUrl(val.url) ) {
-		if( val.type == "text" ) {
-			data.type = "text";
-			data.text = '扫一下，文字瞬间到手机！';
-			data.title = val.text;
-			data.imgUrl = getQrCode( data.title, config.qrCode.type );
-			callback( data );
-		} else if( val.type == "url" ) {
-			data.type = "url";
-			// data.text = val.title;
-			data.text = '扫一下，网址瞬间到手机！';
-			data.title = val.url;
-			if( config.tinyUrl.enabled ) {
-				getTinyUrl( val.url, config.tinyUrl.type, function( tinyurl ) {
-					data.imgUrl = getQrCode( tinyurl, config.qrCode.type );
-					callback( data );
-				});
-			} else {
-				data.imgUrl = getQrCode( val.url, config.qrCode.type );
-				callback( data );
-			}
-		} else if( val.type == "image" ) {
-			data.type = "image";
-			data.text = '扫一下，图片瞬间到手机！';
-			data.title = val.url;
-			if( config.tinyUrl.enabled ) {
-				getTinyUrl( val.url, config.tinyUrl.type, function( tinyurl ) {
-					data.imgUrl = getQrCode( tinyurl, config.qrCode.type );
-					callback( data );
-				});
-			} else {
-				data.imgUrl = getQrCode( val.url, config.qrCode.type );
-				callback( data );
-			}
-		} else if( val.type == "link" ) {
-			data.type = "link";
-			data.text = '扫一下，链接瞬间到手机！';
-			data.title = val.url;
-			if( config.tinyUrl.enabled ) {
-				getTinyUrl( val.url, config.tinyUrl.type, function( tinyurl ) {
-					data.imgUrl = getQrCode( tinyurl, config.qrCode.type );
-					callback( data );
-				});
-			} else {
-				data.imgUrl = getQrCode( val.url, config.qrCode.type );
-				callback( data );
-			}
-		}
-	} else {
-		data = {
-			'type': 'error',
-			'text': '<span class="error">错误：不是有效的网址</span>',
-			'imgUrl': chrome.extension.getURL('images/error.png'),
-			'title': ''
-		}
-		// console.log("Page Error", data );
-		callback( data );
-	}
-}
+            createLinkInfo = {
+                type: 'normal',
+                title: "转化链接为二维码",
+                contexts: ['link'],
+                onclick: function (info, tab) {
+                    creatQrcode({ "type": 'link', "url": tab.url, "text": info.linkUrl}, function (data) {
+                        chrome.tabs.sendMessage(tab.id, {'type': 'qrencode', 'data': data});
+                    });
+                }
+            };
 
+        if (!menuId.text) {
+            menuId.text = chrome.contextMenus.create(createTextInfo);
+        }
+        if (!menuId.image) {
+            menuId.image = chrome.contextMenus.create(createImageInfo);
+        }
+        if (!menuId.link) {
+            menuId.link = chrome.contextMenus.create(createLinkInfo);
+        }
+        // console.log( menuId );
+    },
+
+    // 移除右键菜单
+    destoryContentMenu = function () {
+        chrome.contextMenus.removeAll(function () {
+            menuId = {'image': 0, 'text': 0, 'link': 0};
+        });
+    },
+
+    // 初始化插件
+    initExt = function () {
+
+        // 初始化按钮
+        chrome.browserAction.setPopup({'popup': ''});
+
+        // 初始化右键菜单
+        initContentMenus();
+
+        // 监听二维码事件
+        chrome.browserAction.onClicked.addListener(function (tab) {
+            creatQrcode({"type": 'url', "url": tab.url, "text": tab.url}, function (data) {
+                chrome.tabs.sendMessage(tab.id, {'type': 'qrencode', 'data': data});
+            });
+        });
+
+        // 监听浮层打开事件
+        chrome.extension.onMessage.addListener(function (request) {
+            if (request.type === 'hideContextMenus') {
+                destoryContentMenu();
+            } else if (request.type === 'showContextMenus') {
+                initContentMenus();
+            }
+        });
+    };
 initExt();
