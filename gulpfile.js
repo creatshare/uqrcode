58314c25716b96da5e145e712b3c803f415165bb
AlphaTr <trefoiles@gmail.com>
2014-12-01 14:57:17 +0800

    modify gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,31 @@ var gulp = require('gulp'),
     cssMinify = require('gulp-minify-css'),
     zip = require('gulp-zip');
 
-/* 压缩 CSS，JS */
-gulp.task('minify', function () {
-    gulp.src(['src/**/*.js'], {base: 'src'})
+/* 压缩 JS */
+gulp.task('minifyjs', function () {
+    return gulp.src(['src/**/*.js'], {base: 'src'})
         .pipe(uglify())
         .pipe(gulp.dest('build'));
+});
 
-    gulp.src(['src/**/*.css'], {base: 'src'})
+/* 压缩 CSS */
+gulp.task('minifycss', function () {
+    return gulp.src(['src/**/*.css'], {base: 'src'})
         .pipe(cssMinify())
         .pipe(gulp.dest('build'));
 });
 
+gulp.task('minify', ['minifyjs', 'minifycss']);
+
 /* 复制图片等其他文件 */
 gulp.task('copy', function () {
-    gulp.src(['src/**', '!src/**/*.js', '!src/**/*.css'], {base: 'src'})
+    return gulp.src(['src/**', '!src/**/*.js', '!src/**/*.css'], {base: 'src'})
         .pipe(gulp.dest('build'));
 });
 
 /* 打包压缩 */
 gulp.task('package', ['minify', 'copy'], function () {
-    gulp.src('build/**')
+    return gulp.src('build/**')
         .pipe(zip('build.zip'))
         .pipe(gulp.dest('./'));
 });
